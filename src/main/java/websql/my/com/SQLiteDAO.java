78a8d6d2065d47911e0fbe2b574142ceab79ae7f
os <osirisbest@gmail.com>
2016-08-14 13:09:21 +0300

    SimpleJdbcInsert

--- src/main/java/websql/my/com/SQLiteDAO.java
+++ src/main/java/websql/my/com/SQLiteDAO.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 @Component("sqliteDAO")
@@ -17,18 +18,29 @@ public class SQLiteDAO {
 	private JdbcTemplate jdbcTemplate;
 	private MapSqlParameterSource par = new MapSqlParameterSource();
 	private String sql;
+	private SimpleJdbcInsert SimpleInsert;
 
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.SimpleInsert = new SimpleJdbcInsert(dataSource).withTableName("mp3").usingColumns("name", "author");
 	}
 
 	public void insert(record rec) {
-		sql = "insert into mp3 (name, author) VALUES (?, ?)";
+		// sql = "insert into mp3 (name, author) VALUES (?, ?)";
 		// KeyHolder kh = new GeneratedKeyHolder();
-		jdbcTemplate.update(sql, new Object[] { rec.getName(), rec.getAuthor() });
+		// jdbcTemplate.update(sql, new Object[] { rec.getName(),
+		// rec.getAuthor() });
 		// return (Integer) kh.getKey();
+		par.addValue("author", rec.getAuthor());
+		par.addValue("name", rec.getName());
+		SimpleInsert.execute(par);
 
+	}
+
+	public void clean() {
+		sql = "delete from mp3";
+		jdbcTemplate.execute(sql);
 	}
 
 	public void deleteID(int id) {
--- src/test/java/websql/my/com/test.java
+++ src/test/java/websql/my/com/test.java
@@ -1,20 +1,32 @@
 package websql.my.com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test {
 
 	public static void main(String[] args) {
 
+		final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("context.xml");
 		SQLiteDAO sqLiteDAO = (SQLiteDAO) ac.getBean("sqliteDAO");
-		System.out.println(sqLiteDAO.getRecordbyID(10).getName());
+		// System.out.println(sqLiteDAO.getRecordbyID(10).getName());
+		logger.debug("clean start");
+		sqLiteDAO.clean();
+		logger.info("clean OK");
 
 		record rec = new record();
-		rec.setAuthor("author");
-		rec.setName("name");
 
-		sqLiteDAO.insert(rec);
+		for (int i = 0; i < 100; i++) {
+			rec.setAuthor("author" + i);
+			rec.setName("name" + i);
+
+			sqLiteDAO.insert(rec);
+		}
+
+		logger.info("insert OK");
 
 	}
 
